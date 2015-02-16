f77df5905109e1b26bde818d1cde726cd4fa6fe5
zaneck <val.owczarek@gmail.com>
2015-02-16 11:39:23 +0100

    demi seance sauvegarde

--- tp4.py
+++ tp4.py
@@ -4,12 +4,18 @@ import matplotlib.pyplot as plt
 #iniit des donnees
 def init():
     global data
+    global Pfemme
+    global Phomme
+    
     
     f=np.loadtxt("data/taillepoids_f.txt")
     h=np.loadtxt("data/taillepoids_h.txt")
     Nf=f.size
     Nh=h.size
 
+    Pfemme=(Nf)/(Nf+Nh)
+    Phomme=(Nh)/(Nf+Nh)
+    
     f=np.vstack((np.ones((1,Nf/2)),f.T))
     h=np.vstack(((np.ones((1,Nh/2))*0),h.T))
 
@@ -20,12 +26,26 @@ def init():
 def oracle(x):
     global data
     return data[0,x]
-    
-# def pasT(pas):
-#    return 0.5/(0.5+pas)
 
-# def nextTheta(theta, pas):
-#     print "not yet"
+def sigmoide(t, A, b):
+    return (1.0/(1.0+np.exp(-((A*t)+b))))
+
+def pasT(pas):
+    return 0.5/(0.5+pas)
+
+def nextTheta(theta, pas):
+    global data
+
+    quartCal=np.dot(data,(data[0] - np.sum(np.dot(data.T, theta))))
+    demiCal=(pasT(pas)/data[0].size)* quartCal
+    
+    return theta + demiCal
+
+def fTheta(theta):
+   global data
+
+   return np.dot(data.T, theta)
+
 
 # def erreurLogis(theta):
 #     print "not yet"
@@ -35,9 +55,25 @@ def oracle(x):
 
 if __name__ == '__main__':
     global data
-    
+    global Pfemme
+    global Phomme
+
+    #init
     init()
-    
-    print data.shape
-    print data
-    #plt.show()
+    theta=np.array([0,0,0])
+
+    theta=nextTheta(theta, 0.5)
+    print theta
+       
+    theta=nextTheta(theta, 0.4)
+    print theta
+
+    theta=nextTheta(theta, 0.4)
+    print theta
+
+    ftheta=fTheta(theta)
+
+    plt.plot(ftheta, '*')
+
+    #plt.plot(data[1], sigmoide(data[1],1,-170),'*')
+    plt.show()
